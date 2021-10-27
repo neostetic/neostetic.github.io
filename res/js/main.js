dcf7c0d12fec4e24bc23fd3f5e0a03d36d41b515
Jan Poláček <gg.polacek@gmail.com>
2021-10-27 15:44:18 +0200

    Delete res directory

--- res/js/main.js
+++
@@ -1,9 +0,0 @@
-function getText(nth) {
-    const para = [
-        "<b>Drape E-sport</b><br>I made logo for them. But unfortunately they got on work multiple people, so my logo didn't get approved by them. Anyways I think it is still piece of art that I like.",
-        "<b>Quill E-sport</b><br> huge payne in the ass. I had to make logo quickly, because their previous designer stole logo. It took me a while, but I got it and it looks awesome.",
-        "<b>Aquaz E-sport Team</b><br> I made this with passion and tried to make a brand based on Aqua (water), they liked it... I sure do too. I think, not my best yet whatever. But sadly, they broke up.",
-        "<b>https://cookieclicker98.github.io</b><br> this was my school project for Web Applications. I'm still working on it, until I will be satisfied... One of my best websites yet. Still proud of it!"
-    ]
-    document.getElementById("projectText").innerHTML = para[nth];
-}
