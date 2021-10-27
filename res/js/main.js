b511f65f2ed65800edaca0073689edd1eb703c7a
Jan Poláček <gg.polacek@gmail.com>
2021-10-27 15:44:58 +0200

    Add files via upload

---
+++ res/js/main.js
@@ -0,0 +1,23 @@
+const popupAnim = "popUp .8s ease";
+const popdowmAnim = "popDown .8s ease";
+
+let about = document.getElementById("about-popup");
+document.getElementById("about-click").onclick = function () {
+    about.style.display = "table";
+    about.style.animation = popupAnim;
+}
+document.getElementById("about-popup-x").onclick = function () {
+    about.style.animation = popdowmAnim;
+    setTimeout(function (){
+        about.style.display = "none";
+    }, 760)
+}
+
+function setMain(id) {
+    document.getElementById("a-home").classList.remove("onpage");
+    document.getElementById("a-github").classList.remove("onpage");
+    document.getElementById("a-projects").classList.remove("onpage");
+    document.getElementById("a-school").classList.remove("onpage");
+    let element = document.getElementById(id);
+    element.classList.add("onpage");
+}
