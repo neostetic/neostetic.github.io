1c742e4bc2031121145d259e893f6b0b8601fb82
Jan Poláček <gg.polacek@gmail.com>
2022-08-12 13:41:08 +0200

    Update Website to new version
    Inspired by https://www.lars-olson.com

---
+++ res/script/main.js
@@ -0,0 +1,19 @@
+window.addEventListener("load", async () => {
+    document.getElementById('loader').style.opacity = '0';
+    await sleep(1000);
+    document.getElementById('loader').style.display = 'none';
+})
+
+let detailsButton = document.getElementById('details-button');
+detailsButton.onclick = () => {
+    let detailsText = document.getElementById('details-text');
+    if (detailsText.style.display !== 'inline') {
+        detailsText.style.display = 'inline';
+        detailsButton.classList.remove('switch-down');
+        detailsButton.classList.add('switch-up');
+    } else {
+        detailsText.style.display = 'none';
+        detailsButton.classList.remove('switch-up');
+        detailsButton.classList.add('switch-down');
+    }
+}
