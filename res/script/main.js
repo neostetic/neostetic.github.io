b4d6df54e6dec62a58b7ef719aa0a26662537ed5
Jan Poláček <jan.polacek@ixperta.com>
2026-03-23 16:16:59 +0100

    Update

--- res/script/main.js
+++ res/script/main.js
@@ -13,7 +13,7 @@ const href = async (url, anim) => {
     await wait(1)
     loader.style.display = "flex"
     loader.style.clipPath = "polygon(0 0, 100% 0, 100% 100%, 0% 100%)"
-    await wait(1)
+    await wait(1000)
     loader.style.clipPath = "polygon(0 0, 100% 0, 100% 0, 0 0)"
     await wait(600)
     loader.style.display = "none"
