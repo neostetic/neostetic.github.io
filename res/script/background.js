7a729eafabfa2482fda296be7bc9570d31891a2a
Jan Poláček <jan.polacek@ixperta.com>
2026-03-24 18:17:57 +0100

    Background Update + Fix

--- res/script/background.js
+++ res/script/background.js
@@ -1,5 +1,7 @@
 window.addEventListener('scroll', () => {
   const scrollY = window.scrollY;
+  let scale_bg = scrollY * 0.005 + 1
+  if (scale_bg < 1) { scale_bg = 1 }
   document.querySelector('.parallax-img').style.transform =
-    `translateY(${scrollY * 0.3}px) scale(${scrollY * 0.005 + 1})`;
+    `translateY(${scrollY * 0.3}px) scale(${scale_bg})`;
 });
